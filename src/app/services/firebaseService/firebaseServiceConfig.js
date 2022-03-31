c3777e206812c007fce3e62b2165dd51abd3764c
Chynara <chipunyaaa@gmail.com>
2022-03-30 23:41:26 -0500

    done

--- src/app/services/firebaseService/firebaseServiceConfig.js
+++ src/app/services/firebaseService/firebaseServiceConfig.js
@@ -17,23 +17,6 @@ const devConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// CONFIGURATION FROM THE FIREBASE ITSELF
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCfPOoTigQufoG5hpyef2o-VkhvC4EqNsY",
-//   authDomain: "semetey-authentication.firebaseapp.com",
-//   projectId: "semetey-authentication",
-//   storageBucket: "semetey-authentication.appspot.com",
-//   messagingSenderId: "150917536545",
-//   appId: "1:150917536545:web:65ebe3503473f3eedefc60"
-// };
-
-// REACT_APP_FIREBASE_API_KEY =AIzaSyCfPOoTigQufoG5hpyef2o-VkhvC4EqNsY
-// REACT_APP_FIREBASE_AUTH_DOMAIN=semetey-authentication.firebaseapp.com
-// REACT_APP_FIREBASE_PROJECT_ID=semetey-authentication
-// REACT_APP_FIREBASE_STORAGE_BUCKET=semetey-authentication.appspot.com
-// REACT_APP_FIREBASE_MESSAGING_SENDER_ID=150917536545
-// REACT_APP_FIREBASE_APP_ID=1:150917536545:web:65ebe3503473f3eedefc60
-
 const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
 
 export default config;
