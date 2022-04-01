e0af6275fca1c0a8887d27aa5c5855790aae4a9c
tagaism <i77tag@gmail.com>
2022-04-02 08:33:42 +0900

    fix prod env var

--- src/app/services/firebaseService/firebaseServiceConfig.js
+++ src/app/services/firebaseService/firebaseServiceConfig.js
@@ -1,11 +1,11 @@
 const prodConfig = {
-  apiKey: process.env.PROD_REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.PROD_REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.PROD_REACT_APP_FIREBASE_DATA_BASE_URL,
-  projectId: process.env.PROD_REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PROD_REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.PROD_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.PROD_REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const devConfig = {
