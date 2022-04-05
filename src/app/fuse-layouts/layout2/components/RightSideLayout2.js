2ca21bd4759e829043e633e822c7cc2fad89a209
dootalievas <dootalievas@gmail.com>
2022-04-05 00:57:13 -0500

    deleted some unnecessary apps from UI and project codebase

--- src/app/fuse-layouts/layout2/components/RightSideLayout2.js
+++ src/app/fuse-layouts/layout2/components/RightSideLayout2.js
@@ -1,18 +1,7 @@
-import ChatPanel from 'app/fuse-layouts/shared-components/chatPanel/ChatPanel';
-import QuickPanel from 'app/fuse-layouts/shared-components/quickPanel/QuickPanel';
-import NotificationPanel from 'app/fuse-layouts/shared-components/notificationPanel/NotificationPanel';
-import { memo } from 'react';
+import { memo } from "react";
 
 function RightSideLayout2() {
-  return (
-    <>
-      <ChatPanel />
-
-      <QuickPanel />
-
-      <NotificationPanel />
-    </>
-  );
+  return <></>;
 }
 
 export default memo(RightSideLayout2);
