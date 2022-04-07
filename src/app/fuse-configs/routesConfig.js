73f788d3ad47dae9bb04e61e42ff534408973b8d
Chynara <chipunyaaa@gmail.com>
2022-04-07 15:32:10 -0500

    remoevd some more unneccessary elements. restores route for error-404 and also copied the folder for this error, but it is not working, i.e. when testing the error-4 component is not rendered

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -1,14 +1,14 @@
-import FuseUtils from '@fuse/utils';
-import appsConfigs from 'app/main/apps/appsConfigs';
-import authRoleExamplesConfigs from 'app/main/auth/authRoleExamplesConfigs';
-import CallbackConfig from 'app/main/callback/CallbackConfig';
-import LoginConfig from 'app/main/login/LoginConfig';
-import LogoutConfig from 'app/main/logout/LogoutConfig';
-import pagesConfigs from 'app/main/pages/pagesConfigs';
-import RegisterConfig from 'app/main/register/RegisterConfig';
+import FuseUtils from "@fuse/utils";
+import appsConfigs from "app/main/apps/appsConfigs";
+import authRoleExamplesConfigs from "app/main/auth/authRoleExamplesConfigs";
+import CallbackConfig from "app/main/callback/CallbackConfig";
+import LoginConfig from "app/main/login/LoginConfig";
+import LogoutConfig from "app/main/logout/LogoutConfig";
+import pagesConfigs from "app/main/pages/pagesConfigs";
+import RegisterConfig from "app/main/register/RegisterConfig";
 
-import { Redirect } from 'react-router-dom';
-import FuseLoading from '@fuse/core/FuseLoading';
+import { Redirect } from "react-router-dom";
+import FuseLoading from "@fuse/core/FuseLoading";
 
 const routeConfigs = [
   ...appsConfigs,
@@ -27,14 +27,23 @@ const routes = [
   // ...FuseUtils.generateRoutesFromConfigs(routeConfigs, ['admin','staff','user']),
   // The individual route configs which has auth option won't be overridden.
   // ...FuseUtils.generateRoutesFromConfigs(routeConfigs, null),
-  ...FuseUtils.generateRoutesFromConfigs(routeConfigs, ['admin', 'staff', 'user']),
+  ...FuseUtils.generateRoutesFromConfigs(routeConfigs, [
+    "admin",
+    "staff",
+    "user",
+  ]),
   {
-    path: '/loading',
+    path: "/",
+    exact: true,
+    component: () => <Redirect to="apps/dashboards/project" />,
+  },
+  {
+    path: "/loading",
     exact: true,
     component: () => <FuseLoading />,
   },
   {
-    component: () => <Redirect to="apps/dashboards/project" />,
+    component: () => <Redirect to="/pages/errors/error-404" />,
   },
 ];
 
