4812f4bf3ee2f7e69bc6b917e561bfc8ebd9b441
ray-nura <raynura182@gmail.com>
2022-04-08 19:17:17 -0700

    returned deleted line

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -3,6 +3,7 @@ import appsConfigs from "app/main/apps/appsConfigs";
 import authRoleExamplesConfigs from "app/main/auth/authRoleExamplesConfigs";
 import CallbackConfig from "app/main/callback/CallbackConfig";
 import LoginConfig from "app/main/login/LoginConfig";
+import ForgotPasswordPageConfig from 'app/main/forgot-password/ForgotPasswordPageConfig';
 import LogoutConfig from "app/main/logout/LogoutConfig";
 import pagesConfigs from "app/main/pages/pagesConfigs";
 import RegisterConfig from "app/main/register/RegisterConfig";
@@ -14,7 +15,7 @@ const routeConfigs = [
   ...appsConfigs,
   ...pagesConfigs,
   ...authRoleExamplesConfigs,
-
+  ForgotPasswordPageConfig,
   LogoutConfig,
   LoginConfig,
   RegisterConfig,
